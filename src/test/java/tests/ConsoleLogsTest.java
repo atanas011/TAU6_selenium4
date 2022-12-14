02f2f890f1e3341603ce5cc518d7da5104d8ff02
atanas011 <atanas011@yahoo.com>
2022-12-14 13:28:37 +0100

    Console Logs

---
+++ src/test/java/tests/ConsoleLogsTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v85.log.Log;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ConsoleLogsTest {
+
+    private EdgeDriver driver; // !!!
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testViewBrowserConsoleLogs() {
+        DevTools devTools = driver.getDevTools();
+        devTools.createSession();
+        devTools.send(Log.enable());
+        devTools.addListener(Log.entryAdded(), logEntry -> {
+            System.out.println("----------");
+            System.out.println("Level: " + logEntry.getLevel());
+            System.out.println("Text: " + logEntry.getText());
+            System.out.println("Broken URL: " + logEntry.getUrl());
+        });
+        driver.get("http://the-internet.herokuapp.com/broken_images");
+        devTools.close(); // !!!
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
